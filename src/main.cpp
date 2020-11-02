540a2fec19f9cb2b744e192b557a4af3d5614f8f
Petr Pécsi <47882869+PetersonAjtakSom@users.noreply.github.com>
2020-11-02 13:03:04 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,35 @@
-int WATERPUMP = 13; //motor pump connected to pin 13
-int sensor = 8; //sensor digital pin vonnected to pin 8
-int val; //This variable stores the value received from Soil moisture sensor.
+int WATERPUMP = 13; 
+int sensor = 8; 
+int val; 
 
 void setup() {
   
-  pinMode(13,OUTPUT); //Set pin 13 as OUTPUT pin
-  pinMode(8,INPUT); //Set pin 8 as input pin, to receive data from Soil moisture sensor.
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  while (! Serial);// wait for serial port to connect. Needed for native USB
+  pinMode(13,OUTPUT); 
+  pinMode(8,INPUT); 
+  
+  Serial.begin(9600); 
+  while (! Serial);
   Serial.println("Speed 0 to 255");
 }
 
 void loop()
   { 
-  if (Serial.available()) //loop to operate motor
+  if (Serial.available()) 
   {
-    int speed = Serial.parseInt(); // to read the number entered as text in the Serial Monitor 
+    int speed = Serial.parseInt(); 
     if (speed >= 0 && speed <= 255)
     {
-      analogWrite(WATERPUMP, speed);// tuns on the motor at specified speed 
+      analogWrite(WATERPUMP, speed);
     }
   }
-  val = digitalRead(8);  //Read data from soil moisture sensor  
+  val = digitalRead(8);  
   if(val == LOW) 
   {
-  digitalWrite(13,LOW); //if soil moisture sensor provides LOW value send LOW value to motor pump and motor pump goes off
+  digitalWrite(13,LOW);
   }
   else
   {
-  digitalWrite(13,HIGH); //if soil moisture sensor provides HIGH value send HIGH value to motor pump and motor pump get on
+  digitalWrite(13,HIGH); 
   }
-  delay(400); //Wait for few second and then continue the loop.
+  delay(400);
 }
