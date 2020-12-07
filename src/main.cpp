414fd119f0bd3eb49b0a18742428ec07d33de7c5
Petr Pécsi <47882869+PetersonAjtakSom@users.noreply.github.com>
2020-12-07 20:20:07 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #define analogPin A0
-#define digitalPin 3
+#define digitalPin 2
 #define vccPin 4
 #define motorPin 7
 
 unsigned long cas = 0;
 
 void setup() {
+ 
   pinMode(analogPin, INPUT);
   pinMode(digitalPin, INPUT);
   pinMode(vccPin, OUTPUT);
   pinMode(motorPin, OUTPUT);
-
   digitalWrite(vccPin, LOW);
   digitalWrite(motorPin, LOW);
 
@@ -32,8 +32,9 @@ void setup() {
 }
 
 void loop() {
-  
+
   if (millis() - cas > 3000) {
+
     digitalWrite(vccPin, HIGH);
     delay(100);
     int analog = analogRead(analogPin);
@@ -43,8 +44,11 @@ void loop() {
     lcd.print("    ");
     lcd.setCursor(2, 1);
     lcd.print(analog);
-    if(analog > 600){
+
+    if(analog > 650){
       digitalWrite(motorPin, HIGH);
+      delay(3000);
+      digitalWrite(motorPin,LOW);
       lcd.setCursor(12, 1);
       lcd.print("ON ");
     }else {
